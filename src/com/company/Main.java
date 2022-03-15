791ac5d5884eb6344173ed66992a42bc78735a86
Xemoz <mati111999@interia.eu>
2022-03-15 17:03:05 +0100

    Add Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Co ");
+        System.out.println("kolwiek");
 	// write your code here
     }
 }
