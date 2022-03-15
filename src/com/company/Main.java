02aee461a2877948c741d1683e30e0da7b69d8bb
Xemoz <mati111999@interia.eu>
2022-03-15 17:16:00 +0100

    Changes in Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Pierwsza linia gałęzi Main");
+        System.out.println("Druga linia gałęzi Main");
     }
 }
